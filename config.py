13eef5d563651b2814be9a5f8d8f1092f62b2dd3
d-beloved <morayodeji@gmail.com>
2021-02-06 12:43:18 +0100

    fix import error

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
   TESTING = False
   CSRF_ENABLED = True
   SECRET_KEY = 'gonna change this soon'
-  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_RUL']
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
   DEBUG = False
