b6dfe54b48fe1a4627219f3e0538ed5fe5091f32
d-beloved <morayodeji@gmail.com>
2021-03-05 21:47:45 +0100

    add get job

--- app.py
+++ app.py
@@ -78,9 +78,16 @@ def index():
     )
     print(job.get_id)
 
-@app.route
+    return render_template('index.html', results=results)
 
-  return render_template('index.html', results=results)
+@app.route("/results/<job_key", methods=['GET'])
+def get_results(job_key):
+
+  job = Job.fetch(job_key, connection=conn)
+  if job.is_finished:
+    return str(job.result), 200
+  else:
+    return "Nay!", 202
 
 if __name__ == '__main__':
   app.run()
